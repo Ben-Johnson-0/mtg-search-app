61f233a74f7bcecbbb133887de0fbbc7715b7163
Ben Johnson <143573847+Ben-Johnson-0@users.noreply.github.com>
2025-02-23 16:49:16 -0500

    Added Parameter display
    You can now see the current search parameters and add new parameters.

--- src/app/SearchGUI.tsx
+++ src/app/SearchGUI.tsx
@@ -1,26 +1,69 @@
-function SearchParam({ paramTitle, paramName, paramId}: {paramTitle:string, paramName:string, paramId:string}) {
+"use client";
+import { useState } from "react";
+
+/**
+ * Returns a <ul> containing all current search parameters
+ * @param {Array<{}>} searchParameters - An array of JSON search parameters.
+ * @returns a <ul> of search paramters.
+ */
+function ParameterDisplay({ searchParameters }: { searchParameters:Array<{}> }){
+    let i = 0;
     return (
-        <div>
-            <label htmlFor={paramId}>{paramTitle}:</label><br />
-            <input type="text" id={paramId} name={paramName} />
-            <button>Add</button><br />
-        </div>
+        <ul>
+            {searchParameters.map((param) => (
+                <li key={i++}>{JSON.stringify(param)}</li>
+            ))}
+        </ul>
     );
 }
 
+
+/**
+ * @returns A div containing multiple search parameter fields, a current search parameters display, and a Search button.
+ */
 export default function SearchGUI() {
+    // let searchParams = [{ "example": 1 }, { "example": 2 }, { "example": 3 }];
+    const [searchParams, setSearchParams] = useState<{}[]>([]);
+
+    /**
+     * Returns a div containing a Label, a text input, and an Add button. Used for adding search parameters.
+     * @param {string} paramTitle - The text of the label.
+     * @param {string} paramId - The unique ID for the parameter. (i.e. 'name' for searching via name)
+     * @returns a div with a label, text input, and Add button
+     */
+    function SearchParam({ paramTitle, paramId, inputType }: { paramTitle: string, paramId: string, inputType: string }) {
+        const [inputVal, setInputVal] = useState("");
+
+        function addParameter() {
+            const newParam = {"searchParam": paramId, "searchValue": inputVal}
+            setSearchParams([...searchParams, newParam]);
+        }
+        return (
+            <div>
+                <label htmlFor={paramId}>{paramTitle}:</label><br />
+                <input type={inputType} id={paramId} name={paramId} onChange={(e) => setInputVal(e.target.value)}/>
+                <button onClick={addParameter}>Add</button><br />
+            </div>
+        );
+    }
+
     return (
         <div className= "border-[5px]">
-            <h1>Search</h1>
-            <SearchParam paramTitle="Card Name" paramName="name" paramId="name" />
-            <SearchParam paramTitle="Rules Text" paramName="text" paramId="text" />
-            <SearchParam paramTitle="Expansion" paramName="expansion" paramId="expansion" />
-            <SearchParam paramTitle="Format" paramName="format" paramId="format" />
-            <SearchParam paramTitle="Colors" paramName="colors" paramId="colors" />
-            <SearchParam paramTitle="Color Identity" paramName="colorId" paramId="colorId" />
-            <SearchParam paramTitle="Types" paramName="types" paramId="types" />
-            <SearchParam paramTitle="Subtypes" paramName="subtypes" paramId="subtypes" />
-            {/* <SearchParam paramTitle="Mana Value" paramName="mv" paramId="mv" /> */}
+            <div>
+                <h1>Search Parameters</h1>
+                <SearchParam paramTitle="Card Name" paramId="name" inputType="text" />
+                <SearchParam paramTitle="Rules Text" paramId="oracle_text" inputType="text" />
+                <SearchParam paramTitle="Expansion" paramId="expansion" inputType="text" />
+                <SearchParam paramTitle="Colors" paramId="colors" inputType="text" />
+                <SearchParam paramTitle="Color Identity" paramId="colorId" inputType="text" />
+                <SearchParam paramTitle="Types" paramId="types" inputType="text" />
+                <SearchParam paramTitle="Subtypes" paramId="subtypes" inputType="text" />
+                <SearchParam paramTitle="Mana Value" paramId="mv" inputType="number" />
+            </div>
+            <div>
+                <h1>Current Parameters:</h1>
+                <ParameterDisplay searchParameters={searchParams} />
+            </div>
         </div>
     );
 }

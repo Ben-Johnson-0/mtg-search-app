36f6c28b0da04c26595f5fbdead34190c83ad79a
Ben Johnson <143573847+Ben-Johnson-0@users.noreply.github.com>
2025-04-26 23:56:06 -0400

    Update MultiCardDisplay.tsx
    Added additional data for displaying. Next page button is WIP

--- src/app/MultiCardDisplay.tsx
+++ src/app/MultiCardDisplay.tsx
@@ -1,3 +1,4 @@
+'use client';
 import Card from "./Card"
 
 type CardData = {
@@ -10,17 +11,23 @@ type CardData = {
  * Display widget for a group of cards
  */
 export default function MultiCardDisplay({ cards }: { cards:Array<CardData> }) {
+    let page_num:number = 0;
+    const page_length = 60;
 
     if (!cards || !Array.isArray(cards)) return <div>Loading...</div>;
-    // const cardArr = await cards
-    console.log(Array.isArray(cards), cards);
+    console.log("Cards", cards)
+    console.log("Cards.length", cards.length)
+
     return (
         <div>
+            <h1>{cards.length} Results</h1>
+            <h1>Page {page_num+1} of {Math.ceil(cards.length / page_length)}</h1>
+            <button onClick={(e) => page_num++}>Next</button>
             {
-            cards.map((card) => (
+                cards.slice(page_num * page_length, (page_num + 1) * page_length).map((card) => (
                 <Card 
                     key = {card["_id"]}
-                    img = {card["image_uris"]["normal"]} 
+                    img={card["image_uris"] ? (card["image_uris"]["normal"]) : ("https://cards.scryfall.io/large/front/a/3/a3da3387-454c-4c09-b78f-6fcc36c426ce.jpg")} 
                     name = {card["name"]}
                 />
             ))

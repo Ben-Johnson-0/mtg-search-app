50f04c103b245acd5fe2edc15099880edeaa1d6f
Ben Johnson <143573847+Ben-Johnson-0@users.noreply.github.com>
2025-04-28 15:54:47 -0400

    Separated fetch into its own script
    Moved fetch function and variables to their own file called fetch.ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,7 @@
-import Card from "./Card"
+// Home Page
 import MultiCardDisplay from "./MultiCardDisplay";
 import SearchGUI from "./SearchGUI"
-
-const { MongoClient } = require("mongodb");
-
-// Config
-const mongoIP = "192.168.1.75";
-const mongoPort = 27017;
-const username = "viewer";
-const password = "simViewer2";
-const authDb = "mtg";
-const targetDb = "mtg";
-const collectionName = "similarity";
-
-// Card Data prototype
-type CardData = {
-  _id: number;
-  image_uris: { normal: string };
-  name: string;
-}
-
-// Connection URI
-const uri = `mongodb://${username}:${password}@${mongoIP}:${mongoPort}/?authSource=${authDb}`;
-
-async function fetchCards() {
-  const client = new MongoClient(uri);
-
-  try {
-    await client.connect();
-    const db = client.db(targetDb);
-    const collection = db.collection(collectionName);
-
-    const doc: Array<CardData> = await collection.find({ "cmc": {"$gte": 10}}).toArray(); // Placeholder query
-    console.log("Found document:", doc);
-    return doc;
-
-  } catch (err) {
-    console.error("MongoDB error:", err);
-  } finally {
-    await client.close();
-  }
-
-  console.log("Failed to retrieve any documents.");
-  return [];
-}
+import fetchCards from "./fetch"
 
 
 export default async function Home() {
